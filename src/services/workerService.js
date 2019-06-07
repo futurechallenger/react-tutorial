88619a7cbb0bfbd16163f979ea618e0a8d021afb
Bruce Li <bruce.li@ringcentral.com>
2019-06-07 21:58:57 +0800

    mod

--- src/services/workerService.js
+++ src/services/workerService.js
@@ -1,7 +1,9 @@
-export default function runWorker(data) {
+export default function runWorker(workerPath='../workers/someTask.js', data) {
   return new Promise((resolve, reject) => {
     const worker = new Worker('../workers/someTask.js', {type: 'module'});
 
+    // This may cause `module script does not support` error.
+    // const worker = new Worker(workerPath, {type: 'module'}); 
     worker.onmessage = (event) => {
       console.log('===>event from worker', event);
       resolve(event.data);
